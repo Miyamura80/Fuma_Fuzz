7cc5d928a1131c5f2d450c7909196e27f5578b19
Miyamura80 <eitomiyamura@gmail.com>
2023-01-08 20:06:49 +0000

    now can compile entire directories

--- src/utils/source.py
+++ src/utils/source.py
@@ -1,7 +1,8 @@
 import os.path as osp
 import json
 import solcx
-from typing import Dict, Any
+import glob
+from typing import Dict, Any, List
 
 def get_bytecode(args, root_dir) -> Dict[str, Any]:
     source_folders = args.source.split("/")
@@ -31,3 +32,57 @@ def get_bytecode(args, root_dir) -> Dict[str, Any]:
         return compiled_json
     else:
         raise ValueError("Please provide a .sol type for source")
+
+# e.g. if args.source = "data/{IDENTIFIER}/" 
+# It will compile the jsons to "data/compiled_solidity/{IDENTIFIER}"
+def compile_dir(args, root_dir) -> List[Dict[str, Any]]:
+    source_folders = args.source.split("/")
+    source_folders = source_folders if source_folders[-1]!="" else source_folders[:-1]
+    source_folder_prefix = source_folders[-1]
+    source_dir_path = (osp.join(root_dir, *source_folders))
+
+
+    source_files = glob.glob(source_dir_path+"/*.sol")
+
+    compiled_jsons = []
+    
+    for source_file_path in source_files:
+
+        source_prefix, extention = osp.splitext(osp.basename(source_file_path))
+        precompiled_solidity_path = osp.join(root_dir, "data", "compiled_solidity", f"{source_folder_prefix}",f"{source_prefix}.json")
+
+        if not osp.exists(precompiled_solidity_path) or args.recompile:
+            try:
+                compiled_json = solcx.compile_files(
+                    [source_file_path], # BACKLOG: edit by doing it by feeding all files here
+                    output_values=["abi", "bin-runtime"],
+                    solc_version=args.version,
+                    optimize=args.optimize
+                )
+            except Exception as e:
+                if e.return_code==0:
+                    continue
+                elif e.return_code==1:
+                    with open(source_file_path, "r") as f:
+                        lines = f.readlines()
+                    j = next(i for i,v in enumerate(lines) if v.startswith("pragma"))
+
+                    lines[j] = f"pragma solidity {args.version};\n"
+                    with open(source_file_path, "w") as f:
+                        f.writelines(lines)
+                    compiled_json = solcx.compile_files(
+                        [source_file_path], # BACKLOG: edit by doing it by feeding all files here
+                        output_values=["abi", "bin-runtime"],
+                        solc_version=args.version,
+                        optimize=args.optimize
+                    )
+                
+            compiled_json = compiled_json[list(compiled_json.keys())[0]]
+            with open(precompiled_solidity_path, "w") as f:
+                f.write(json.dumps(compiled_json))
+        else:
+            with open(precompiled_solidity_path, "r") as f:
+                compiled_json = json.load(f)
+                compiled_json = compiled_json[list(compiled_json.keys())[0]]
+        compiled_jsons.append(compiled_json)
+    return compiled_jsons
