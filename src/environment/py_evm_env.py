00cf5fded085c155162041357c268772d3c70a27
Miyamura80 <eitomiyamura@gmail.com>
2023-01-14 16:36:31 +0000

    added observation to pyenv environment

--- src/environment/py_evm_env.py
+++ src/environment/py_evm_env.py
@@ -20,12 +20,15 @@ ASSUMPTIONS:
 
 @struct.dataclass
 class EnvState:
-    abi_info: chex.Array
+    abi_function_onehot: chex.Array
+    abi_argument_onehot: chex.Array
     attacker_balance: int
     contract_balance: int
     view_function_output: chex.Array
     time: int
     # contract: chex.Array # TODO: we ignore this for now!
+    # victim_balances: int # TODO: for later! 
+
 
 
 @struct.dataclass
@@ -36,19 +39,23 @@ class EnvParams:
     max_attack_time: int = 10 # Number of function invocations done before finishing
 
     # Action - Function argument settings
-    address_set_size: int = 2 # Default |A|=2, A[0] is creator, A[1] is attacker
-    uint256_arg_min: int = 1 # 1 gwei
-    uint256_arg_max: int = 8 # 1 ETH
+    uint256_arg_min: int = 0 # 1 gwei
+    uint256_arg_max: int = 10 # 1 ETH
 
 
 # State - Function description settings (ERROR if contract exceeds this)
 MAX_FUNC_VIEWABLE: int = 10 
 MAX_FUNC_NONPAYABLE: int = 10
 MAX_FUNC_PAYABLE: int = 5
+MAX_FUNC_TOTAL: int = MAX_FUNC_VIEWABLE + MAX_FUNC_NONPAYABLE + MAX_FUNC_PAYABLE
+
 MAX_ARGUMENT_COUNT: int = 3
 
+# Action - Function argument settings
+ADDRESS_SET_SIZE: int = 2 # Default |A|=2, A[0] is attacker, A[1] is contract, A[2],... is victims
 
-class PyEVM(environment.Environment):
+
+class PyEVM_Env(environment.Environment):
     """
     JAX Compatible version of EVM environment for RL agent
 
@@ -59,14 +66,17 @@ class PyEVM(environment.Environment):
     - Contract ABI information (order the payable & nonpayable attributes first)
     - Attacker Balance
     - Contract Balance
+    - Victim(s) Balances
     - Time
     - Dynamic State
         - Return results of all `view` functions (not pure, since it doesn't read state)
     
     >>> (
             f_p1, ..., f_p|f_p|, f_n1, ..., f_n|f_n|, f_v1, ..., f_n|f_v|,
-            attacker_balance,
-            contract_balance,
+            attacker_balance (A[0]),
+            contract_balance (A[1]),
+            A[2], A[3], A[4], ... A[|A|-1],
+            time,
             o(f_v1), o(f_v2), ... , o(f_n|f_v|)
         )
     ----------------------------------------------------------------
@@ -88,11 +98,22 @@ class PyEVM(environment.Environment):
     - Termination if contract is entirely drained or max_attack_time reached
 
     """
-
+    # TODO: Collapse this into args
     def __init__(self, contract_abi, bytecode):
         self.contract_abi = contract_abi
-        self.func_num = len(filter(lambda x: x["stateMutability"]=="payable" or x["stateMutability"]=="nonpayable", contract_abi))
+        self.action_func_num = len(filter(lambda x: x["stateMutability"]=="payable" or x["stateMutability"]=="nonpayable", contract_abi))
         self.bytecode = bytecode
+        
+        # Setting up the observation attributes
+        temp_params = self.default_params()
+        arg_int_size = temp_params.uint256_arg_max - temp_params.uint256_arg_min + 1
+        
+        self.arg_int_size = arg_int_size
+        self.obs_shape = (
+            6, 
+            MAX_FUNC_NONPAYABLE+MAX_FUNC_PAYABLE+MAX_FUNC_VIEWABLE, 
+            MAX_ARGUMENT_COUNT*(ADDRESS_SET_SIZE+arg_int_size)
+        )
         super().__init__()
 
     @property
@@ -127,11 +148,14 @@ class PyEVM(environment.Environment):
         """Reset environment state by sampling initial position."""
         if params is None:
             params = self.default_params
+        abi_function_onehot, abi_argument_onehot = contract_abi_json_to_array(self.contract_abi)
+        dynamic_observation = reset_dynamic_observation(self.contract_abi, params)
         state = EnvState(
-            abi_info=contract_abi_json_to_array(self.contract_abi),
+            abi_function_onehot=abi_function_onehot,
+            abi_argument_onehot=abi_argument_onehot,
             attacker_balance=10**params.attacker_initial_balance,
             contract_balance=10**params.contract_initial_balance,
-            view_function_output=jnp.zeros([]), # TODO: Requires quite a bit of thinking
+            view_function_output=dynamic_observation, 
             time=0,
         )
         return self.get_obs(state), state
@@ -143,23 +167,49 @@ class PyEVM(environment.Environment):
             contract_balance,
             o(f_v1), o(f_v2), ... , o(f_n|f_v|)
         )
+
+        Dim 0: Info of function presence in ABI
+            - e.g.  I(f_p1), ..., I(f_p|f_p|), 
+                    I(f_n1), ..., I(f_n|f_n|), 
+                    I(f_v1), ..., I(f_n|f_v|)
+            - Shape: (|f_p| + |f_n| + |f_v|, 1)
+        Dim 1: Info of argument types (address)
+            - e.g.  I(c^1_p1==addr), ..., I(c^1_p|f_p|==addr),
+                    I(c^1_n1==addr), ..., I(c^1_n|f_n|==addr),
+                    I(c^1_v1==addr), ..., I(c^1_v|f_v|==addr),
+            - Shape: (|f_p| + |f_n| + |f_v|, m)
+        Dim 2: Info of argument types (uint256)
+            - e.g.  I(c^1_p1==uint256), ..., I(c^1_p|f_p|==uint256)
+                    I(c^1_n1==uint256), ..., I(c^1_n|f_n|==uint256)
+                    I(c^1_v1==uint256), ..., I(c^1_v|f_v|==uint256)
+            - Shape: (|f_p| + |f_n| + |f_v|, m)
+        Dim 3: Balances
+            - e.g.  A[0]
+                    A[1]
+            - Shape: (|A|, 1)
+        Dim 4: Time
+            - e.g.  t
+            - Shape: (1,1)
+        Dim 5: Dynamic observation (view function output)
+            - e.g.  O(f_v1)
+                    O(f_v2)
+                    ...
+                    O(f_v|f_v|)
+            - Shape: (|f_v|, m*(|I|+|A|))
     """
     def get_obs(self, state: EnvState) -> chex.Array:
         """Return observation from raw state trafo."""
-        payable_func_dim = 0
-        nonpayable_func_dim = 0
-        view_func_dim = 0
+        obs = jnp.zeros(self.obs_shape, dtype=jnp.int64)
+        # Set the observation tensor according to above spec
+        obs = obs.at[0, :, 0].set(state.abi_function_onehot)
+        obs = obs.at[1, :, 0:MAX_ARGUMENT_COUNT].set(state.abi_argument_onehot.at[0, :, :])
+        obs = obs.at[2, :, 0:MAX_ARGUMENT_COUNT].set(state.abi_argument_onehot.at[1, :, :])
+        # BACKLOG: Seperate this into seperate dimensions
+        obs = obs.at[3, 0:ADDRESS_SET_SIZE, 0].set(jnp.array([state.attacker_balance, state.contract_balance]))
+        obs = obs.at[4, 0, 0].set(state.time)
+        obs = obs.at[5, :MAX_FUNC_VIEWABLE, MAX_ARGUMENT_COUNT*(self.arg_int_size + ADDRESS_SET_SIZE)].set(state.view_function_output)
 
-        # TODO: Implement this
-
-        # obs = jnp.zeros(self.obs_shape, dtype=bool)
-        # # Set the position of the player paddle, paddle, trail & brick map
-        # obs = obs.at[9, state.pos, 0].set(1)
-        # obs = obs.at[state.ball_y, state.ball_x, 1].set(1)
-        # obs = obs.at[state.last_y, state.last_x, 2].set(1)
-        # obs = obs.at[:, :, 3].set(state.brick_map)
-        # return obs.astype(jnp.float32)
-        
+        return obs.astype(jnp.float64)
 
     def is_terminal(self, state: EnvState, params: EnvParams) -> bool:
         """Check whether state is terminal."""
@@ -188,37 +238,70 @@ class PyEVM(environment.Environment):
             params = self.default_params
 
         # Where c_i from [0,10] -> maps to uint256, c_i from [11,12] -> maps to addresses
-        argument_index = 10+params.address_set_size
+        # >>> (function_id, payment_gwei, c_1, c_2, c_3)
+        argument_index = 10+ADDRESS_SET_SIZE
         low = jnp.array(
             [0, 0, 0, 0, 0], dtype=jnp.int64
         )
         high = jnp.array(
-            [self.func_num-1, params.attacker_initial_balance, 
+            [self.action_func_num-1, params.attacker_initial_balance, 
                 argument_index, argument_index, argument_index], dtype=jnp.int64
         )
         return spaces.Box(low, high, (5,), jnp.int64)
 
-    # TODO
     def observation_space(self, params: EnvParams) -> spaces.Box:
         """Observation space of the environment."""
-        return spaces.Box(0, 1, self.obs_shape)
+        return spaces.Box(0, 10**params.attacker_initial_balance, self.obs_shape, jnp.int64)
 
     def state_space(self, params: EnvParams) -> spaces.Dict:
         """State space of the environment."""
+
+        (num_dim, height, width) = self.obs_shape
+        view_function_output_width = MAX_ARGUMENT_COUNT*(self.arg_int_size + ADDRESS_SET_SIZE)
         return spaces.Dict(
             {
-                "abi_info": spaces.Box(0,1 (10,10)),
-                "attacker_balance": spaces.Box(0,10**params.attacker_initial_balance, (1,), dtype=jnp.int64),
-                "contract_balance": spaces.Box(0,10**params.attacker_initial_balance, (1,), dtype=jnp.int64),
-                "view_function_output": spaces.Box(0,1 (10,10)),
-                "time": spaces.Discrete(params.max_attack_time),
+                "abi_function_onehot": spaces.Box(0, 1, (height, 1)),
+                "abi_argument_onehot": spaces.Box(0, 1, (2, height, MAX_ARGUMENT_COUNT)),
+                "attacker_balance": spaces.Box(0, jnp.finfo(jnp.int64).max, (1,), dtype=jnp.int64),
+                "contract_balance": spaces.Box(0, jnp.finfo(jnp.int64).max, (1,), dtype=jnp.int64),
+                "view_function_output": spaces.Box(0,jnp.finfo(jnp.int64).max (MAX_FUNC_VIEWABLE, view_function_output_width)),
+                "time": spaces.Discrete(0, params.max_attack_time, (1,), dtype=jnp.int8),
             }
         )
 
 
-# TODO
-def contract_abi_json_to_array(contract_abi: dict) -> chex.Array:
-    pass
+def contract_abi_json_to_array(contract_abi: dict) -> Tuple[chex.Array, chex.Array]:
+    # Assertion about proper contract ABI requirements met
+    f_v_set, f_p_set, f_n_set = [], [], []
+    for func in contract_abi:
+        if func["stateMutability"]=="view":
+            f_v_set.append(func)
+        elif func["stateMutability"]=="payable":
+            f_p_set.append(func)
+        elif func["stateMutability"]=="nonpayable":
+            f_n_set.append(func)
+
+    assert len(f_v_set) <= MAX_FUNC_VIEWABLE
+    assert len(f_p_set) <= MAX_FUNC_PAYABLE
+    assert len(f_n_set) <= MAX_FUNC_NONPAYABLE
+
+    abi_function_onehot = jnp.zeros((MAX_FUNC_TOTAL, 1),jnp.int64) 
+    abi_argument_onehot = jnp.zeros((2, MAX_FUNC_TOTAL, MAX_ARGUMENT_COUNT),jnp.int64)
+
+    f_v_set += [{}] * (MAX_FUNC_VIEWABLE - len(f_v_set))
+    f_p_set += [{}] * (MAX_FUNC_PAYABLE - len(f_p_set))
+    f_n_set += [{}] * (MAX_FUNC_NONPAYABLE - len(f_n_set))
+    whole_list = f_p_set + f_n_set + f_v_set
+
+    argument_to_dim = {"address": 0, "uint256": 1}    
+
+    for i in range(0, MAX_FUNC_TOTAL):
+        if whole_list[i] != {}:
+            abi_function_onehot = abi_function_onehot.at[i, 0].set(1)
+            for j, argument in enumerate(whole_list[i]["inputs"]):
+                abi_argument_onehot = abi_argument_onehot.at[argument_to_dim[argument["type"]], i, j].set(1)
+
+    return abi_function_onehot, abi_argument_onehot
 
 # TODO
 def step_evm(state: EnvState, contract_abi: dict, action: spaces.Box) -> Tuple[EnvState, float]:
@@ -238,6 +321,10 @@ def step_evm(state: EnvState, contract_abi: dict, action: spaces.Box) -> Tuple[E
     reward = 0.0
 
     return state, reward
+
+# TODO
+def reset_dynamic_observation(contract_abi: dict, params: EnvParams):
+    pass
 
 
 
