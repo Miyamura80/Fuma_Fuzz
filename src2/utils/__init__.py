5ea8ad5fe16d50f713ef6e31c088ca372324f35f
Miyamura80 <eitomiyamura@gmail.com>
2023-03-06 21:25:35 +0000

    changed utils formula

--- src2/utils/__init__.py
+++ src2/utils/__init__.py
@@ -1,4 +1,5 @@
 from utils.source import get_bytecode, compile_dir
 from utils.model_loader import get_model
 from utils.config_loader import get_config
-from utils.helper import save_pkl_object, load_model_from_pkl
+# from utils.helper import save_pkl_object, load_model_from_pkl
+from utils.helper import save_pkl_object
