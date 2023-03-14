c6b5777004a357abf485f8f633265d375aca7d25
Miyamura80 <eitomiyamura@gmail.com>
2023-03-14 21:46:44 +0000

    Moved back to JAX: Added evm_helper

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,3 +2,4 @@ from utils.source import get_bytecode, compile_dir
 from utils.model_loader import get_model
 from utils.config_loader import get_config
 from utils.helper import save_pkl_object, load_model_from_pkl
+from utils.evm_helper import keccak_of_func_signature, pad_argument
