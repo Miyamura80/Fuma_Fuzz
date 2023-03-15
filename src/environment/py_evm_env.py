effe45390e7739d82e8c6dbbd894781c20c3c6a6
Miyamura80 <eitomiyamura@gmail.com>
2023-03-15 03:54:45 +0000

    basic minimal version that runs a single training PPO loop

--- src/environment/py_evm_env.py
+++ src/environment/py_evm_env.py
@@ -2,13 +2,20 @@ import jax
 import jax.numpy as jnp
 from jax import lax
 from gymnax.environments import environment, spaces
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 import chex
 from flax import struct
 from evm import Py_EVM, EVM
-from utils import get_bytecode
 import os.path as osp
 from dotmap import DotMap
+from utils import get_bytecode
+from functools import partial
+from jax import jit
+from Crypto.Hash import keccak
+from typing import List
+from eth_utils import encode_hex
+
+
 
 """
 ASSUMPTIONS:
@@ -30,8 +37,8 @@ class EnvState:
     contract_balance: int
     view_function_output: chex.Array
     time: int
-    # contract: chex.Array # TODO: we ignore this for now!
-    # victim_balances: int # TODO: for later! 
+    # contract: chex.Array # BACKLOG: we ignore this for now!
+    # victim_balances: int # BACKLOG: for later! 
 
 @struct.dataclass
 class EnvParams:
@@ -56,6 +63,10 @@ MAX_ARGUMENT_COUNT: int = 3
 # Action - Function argument settings
 ADDRESS_SET_SIZE: int = 2 # Default |A|=2, A[0] is attacker, A[1] is contract, A[2],... is victims
 
+ADDRESSES = {
+                "0x69420552091a69125d5dfcb7b8c2659029395bdf": 0, 
+                "0x38644552091a69125d5dfcb7b8c2659029395bdf": 1, 
+            }
 
 class PyEVM_Env(environment.Environment):
     """
@@ -128,13 +139,13 @@ class PyEVM_Env(environment.Environment):
         arg_int_size = temp_params.uint256_arg_max - temp_params.uint256_arg_min + 1
         
         # EVM 
-        self.evm = Py_EVM(args)
+        self.evm = Py_EVM(args, self.bytecode, self.contract_abi)
 
         self.arg_int_size = arg_int_size
         self.obs_shape = (
-            6, 
+            5, 
             MAX_FUNC_NONPAYABLE+MAX_FUNC_PAYABLE+MAX_FUNC_VIEWABLE, 
-            MAX_ARGUMENT_COUNT*(ADDRESS_SET_SIZE+arg_int_size)
+            max(MAX_ARGUMENT_COUNT, ADDRESS_SET_SIZE)
         )
         super().__init__()
 
@@ -151,8 +162,7 @@ class PyEVM_Env(environment.Environment):
         params: EnvParams,
     ) -> Tuple[chex.Array, EnvState, float, bool, dict]:
         """Perform single timestep state transition."""
-        compilation_result = {"abi": self.contract_abi, "bin-runtime": self.bytecode}
-        state, reward = step_evm(state, compilation_result, action, self.evm, self.action_function_lookup)
+        state, reward = step_evm(params, state, self.contract_abi, action, self.evm, self.action_function_lookup)
 
         # Check game condition & no. steps for termination condition
         done = self.is_terminal(state, params)
@@ -160,7 +170,7 @@ class PyEVM_Env(environment.Environment):
         return (
             lax.stop_gradient(self.get_obs(state)),
             lax.stop_gradient(state),
-            reward.astype(jnp.float32),
+            jnp.float32(reward),
             done,
             info,
         )
@@ -171,8 +181,9 @@ class PyEVM_Env(environment.Environment):
         """Reset environment state by sampling initial position."""
         if params is None:
             params = self.default_params
+        self.evm.reset()
         abi_function_onehot, abi_argument_onehot = contract_abi_json_to_array(self.contract_abi)
-        dynamic_observation = reset_dynamic_observation(self.contract_abi, params)
+        dynamic_observation = reset_dynamic_observation(self.contract_abi, params, self.evm)
         state = EnvState(
             abi_function_onehot=abi_function_onehot,
             abi_argument_onehot=abi_argument_onehot,
@@ -226,12 +237,11 @@ class PyEVM_Env(environment.Environment):
         # Set the observation tensor according to above spec
         obs = obs.at[0, :, 0].set(state.abi_function_onehot)
         obs = obs.at[1:3, :, 0:MAX_ARGUMENT_COUNT].set(state.abi_argument_onehot)
-        # BACKLOG: Seperate this into seperate dimensions
         obs = obs.at[3, 0:ADDRESS_SET_SIZE, 0].set(jnp.array([state.attacker_balance, state.contract_balance]))
-        obs = obs.at[4, 0, 0].set(state.time)
-        obs = obs.at[5, :MAX_FUNC_VIEWABLE, MAX_ARGUMENT_COUNT*(self.arg_int_size + ADDRESS_SET_SIZE)].set(state.view_function_output)
+        obs = obs.at[4, :MAX_FUNC_VIEWABLE, :ADDRESS_SET_SIZE].set(state.view_function_output)
 
         return obs.astype(jnp.float64)
+
 
     def is_terminal(self, state: EnvState, params: EnvParams) -> bool:
         """Check whether state is terminal."""
@@ -292,26 +302,16 @@ class PyEVM_Env(environment.Environment):
         )
 
 # Takes the contract_abi and returns indexing from func_id -> func_name
-def create_function_lookup(contract_abi: dict) -> Tuple[dict, dict]:
-    f_v_set, f_action_set = [], []
-    for func in contract_abi:
-        if func["stateMutability"]=="view":
-            f_v_set.append(func)
-        elif func["stateMutability"]=="payable" or func["stateMutability"]=="nonpayable":
-            f_action_set.append(func)
+def create_function_lookup(contract_abi: dict) -> Tuple[chex.Array, chex.Array]:
+    # These are the sets that are viewable, or actionable
+    f_v_set = [func for func in contract_abi if func["stateMutability"]=="view"]
+    f_action_set = [func for func in contract_abi if func["stateMutability"]=="payable" or func["stateMutability"]=="nonpayable"]
     
     f_v_set += [{}] * (MAX_FUNC_VIEWABLE - len(f_v_set))
     f_action_set += [{}] * (MAX_FUNC_PAYABLE + MAX_FUNC_NONPAYABLE - len(f_action_set))
 
-    action_function_lookup = {}
-    dynamic_obs_func_lookup = {}
-    for i in range(MAX_FUNC_PAYABLE+MAX_FUNC_NONPAYABLE):
-        if f_action_set[i] != {} and "name" in f_action_set[i]:
-            action_function_lookup[i] = f_action_set[i]["name"]
-
-    for i in range(MAX_FUNC_VIEWABLE):
-        if f_v_set[i] != {} and "name" in f_v_set[i]:
-            dynamic_obs_func_lookup[i] = f_v_set[i]["name"]
+    action_function_lookup = jnp.array([get_f_hash_int(contract_abi, f_action_set[i]["name"]) if f_action_set[i] != {} and "name" in f_action_set[i] else 0 for i in range(MAX_FUNC_PAYABLE+MAX_FUNC_NONPAYABLE)])
+    dynamic_obs_func_lookup = jnp.array([get_f_hash_int(contract_abi, f_v_set[i]["name"]) if f_v_set[i] != {} and "name" in f_v_set[i] else 0 for i in range(MAX_FUNC_VIEWABLE)])
 
     return action_function_lookup, dynamic_obs_func_lookup
 
@@ -349,41 +349,97 @@ def contract_abi_json_to_array(contract_abi: dict) -> Tuple[chex.Array, chex.Arr
     return abi_function_onehot, abi_argument_onehot
 
 # TODO
-def step_evm(state: EnvState, compilation_result: dict, action: spaces.Box, evm: EVM, fname_lookup: dict) -> Tuple[EnvState, float]:
-    
+def step_evm(params: EnvParams, state: EnvState, contract_abi: dict, action: spaces.Box, evm: EVM, fname_lookup: chex.Array) -> Tuple[EnvState, float]:
     # Unpack the values from the state
-    func_id, payment_gwei, c_1, c_2, c_3 = action
-
-    func_name = fname_lookup[func_id]
-    # arg_val_lookup = {i: }
+    # func_id, payment_gwei, c_1, c_2, c_3 = action
+    # func_hash: int = jnp.array(fname_lookup)[func_id]
 
     inputs = {}
     args = {}
-    abi_args = (func_name, inputs)
 
     # Execute the action on the EVM
     # evm.run_bytecode(args, compilation_result, abi_args)
 
     new_attacker_balance = state.attacker_balance
     new_contract_balance = state.contract_balance
-    evm_state_obs = []
+    dynamic_observation = reset_dynamic_observation(contract_abi, params, evm)
 
 
     state = EnvState(
-        state.abi_info,
+        state.abi_function_onehot,
+        state.abi_argument_onehot,
         new_attacker_balance,
         new_contract_balance,
-        evm_state_obs,
+        dynamic_observation,
         state.time + 1,
     )
     reward = 0.0
 
     return state, reward
 
-# TODO
-def reset_dynamic_observation(contract_abi: dict, params: EnvParams):
-    pass
+def get_f_hash_int(contract_abi: dict, f_name: str) -> int:
+    # Find the function input signature
+    func = [func for func in contract_abi if "name" in func and func["name"]==f_name][0]
+    input_types = [inp["type"] for inp in func["inputs"]]
+    f_hash = keccak_of_func_signature(f_name, input_types)
+    return int(f_hash.decode(), 16)
 
+powers_of_10 = {i: 10**i for i in range(0, 10)}
+def get_hash_function_call(contract_abi: dict, f_name: str, arguments: List[int], params: EnvParams) -> str:
+    # Find the function input signature
+    func = [func for func in contract_abi if "name" in func and func["name"]==f_name][0]
+    input_types = [inp["type"] for inp in func["inputs"]]
+    f_hash = keccak_of_func_signature(f_name, input_types)
+    
+    arg_in_values = []
+    for i,arg in enumerate(arguments):
+        if i < len(input_types)-1:
+            if arg <= params.arg_int_size and input_types[i]=="uint256":
+                c_i = powers_of_10[arg]
+            elif arg < params.arg_int_size + ADDRESS_SET_SIZE and input_types[i]=="address":
+                c_i = ADDRESSES.keys()[arg - params.arg_int_size]
+            else:
+                c_i = 0
+
+            c_i = pad_argument(c_i)
+            arg_in_values.append(c_i)
+    
+    f_arg = f_hash + b''.join(arg_in_values)
+    return f_arg.decode()
+
+
+def keccak_of_func_signature(func_name: str, param_types: List[str]):
+    # Create a new Keccak-256 hash object
+    h = keccak.new(digest_bits=256)
+    input_str = func_name + "(" + ",".join(param_types) + ")"
+    h.update(str.encode(input_str))
+    hex_hash = h.hexdigest()
+    return hex_hash[0:8].encode('utf-8')
+
+def pad_argument(arg: any):
+    if arg.startswith('0x'):
+        arg = arg[2:]
+    encoded = arg.encode('utf-8')
+    length = len(encoded)
+    padded_encoded = b'0' * (64 - length) + encoded \
+        if length < 64 else encoded
+    return padded_encoded
+
+
+# BACKLOG: With int inputs as well
+def reset_dynamic_observation(contract_abi: dict, params: EnvParams, evm: EVM) -> chex.Array:
+    viewable_abi = [func for i,func in enumerate(contract_abi) if func['stateMutability']=='view']
+    f_hash_typemap = [(keccak_of_func_signature(func['name'],[inp["type"] for inp in func['inputs']]), [inp["type"] for inp in func['inputs']], func['name'], func['outputs'][0]['type']) for func in viewable_abi]
+
+    result = jnp.zeros((MAX_FUNC_VIEWABLE, ADDRESS_SET_SIZE), dtype=jnp.int64)
+    for i, (f_hash, inps, f_name, return_type) in enumerate(f_hash_typemap):
+        for j,addr in enumerate(evm.accounts):
+            f_args = [] if len(inps)==0 else [addr]
+            (return_output, r_type) = evm.run_bytecode(f_name, f_args, 0)
+            # TODO: Gotta handle the case where it returns address
+            result.at[i, j].set(return_output)
+
+    return result
 
 
 
--- src/evm/py_evm.py
+++ src/evm/py_evm.py
@@ -1,5 +1,8 @@
 from .evm import EVM
-
+from typing import Tuple
+from eth_utils import (
+    decode_hex,
+)
 
 def get_runtime_calldata_from_abi(abi, func_name: str, inputs: dict):
     func_interface = next(filter(lambda e: e.get("name", None) == func_name and e.get("type", None) == "function", abi))
@@ -7,19 +10,37 @@ def get_runtime_calldata_from_abi(abi, func_name: str, inputs: dict):
     func_vals = [inputs[name] for name in func_names]
     return abi, func_name, func_vals
 
+ACC_START = 45_000
 
 class Py_EVM(EVM):
     
-    def __init__(self, args) -> None:
+    def __init__(self, args, bytecode, contract_abi, account_num=2) -> None:
         super().__init__(args)
+        self.bytecode_str = bytecode
+        self.contract_abi = contract_abi
+        self.bytecode = decode_hex(bytecode)
 
-    def run_bytecode(self, args, compilation_result, abi_args) -> None:
-        abi_desc, bytecode = compilation_result["abi"], compilation_result["bin-runtime"]
-        func_name, inputs = abi_args
-        print(abi_desc)
-        print(func_name, inputs)
+        # Create accounts 
+        assert account_num >= 2 # Require at least a deployer and an attacker
+        self.accounts = [ACC_START+i for i in range(account_num)]
+        self.deployer_acc = self.accounts[1]
+        self.attacker_acc = self.accounts[0]
+        
+        # Send money to these initialized accounts
+        self.balances = {addr: 100000 for i,addr in enumerate(self.accounts)}
 
-        calldata = get_runtime_calldata_from_abi(abi_desc, func_name, inputs)
-        print(calldata)
+        self.reset()
+
+    # TODO: implement this 
+    def reset(self):
+        pass
+
+    # TODO: Implement this
+    def run_bytecode(self, f_name, f_args, f_value:int=0, sender_id:int=0) -> Tuple[int, int]:
+        # Returns (result, type) type==0: int, type==1: address
+        # By Default, it executes under the attacker
+        sender_account = self.accounts[sender_id] if sender_id!=0 else self.attacker_acc
+
+        return (0, 0)
 
 
